c4d834f3ac87ddbbf6bf7102d615c29900fba339
siznorias <siznorias@gmail.com>
2025-07-14 17:09:52 +0900

    -initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+Zoom Clone using Node.js + Websocket + WebRTC
---
+++ src/server.js
@@ -0,0 +1,14 @@
+import express from "express"
+
+const app = express();
+
+
+app.set("view engine", "pug");
+app.set("views", __dirname + "/views")
+
+console.log("Hello")
+app.get("/", (req, res)=> res.render("home"))
+const handleListen = () =>
+    console.log(`Listening on http: localhost:3000`)
+
+app.listen(3000);
